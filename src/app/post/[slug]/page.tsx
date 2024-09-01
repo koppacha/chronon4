8f323a46162e2c9d9b50673558dc222c5463c5e0
koppacha <a@chr.mn>
2024-09-01 23:19:33 +0900

    ver.4.00 軽微修正

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -73,7 +73,7 @@ export function generateMetadata({ params }: Params): Metadata {
     return notFound();
   }
 
-  const title = `${post.title} | Next.js Blog Example with ${CMS_NAME}`;
+  const title = `${post.title} - Chrononglyph`;
 
   return {
     title,
